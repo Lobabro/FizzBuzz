7d9212348089d86df07bb59ba940f8a71e7dc18e
Yanis LOBASCIO <yanis.lobascio@leboncoin.loc>
2021-06-21 18:30:31 +0200

    Ajout du Kizz et découpage en plusieurs fonctions.

--- Exo_Fizz-Buzz.py
+++ Exo_Fizz-Buzz.py
@@ -1,24 +1,45 @@
 # Test de Fizz-Buzz
 # Si le nombre est divisible par 3 : on écrit Fizz
 # Si le nombre est divisible par 5 : on écrit Buzz
-# Si le nombre est divisible par 3 et par 5 : on écrit Fizzbuzz
+# Si le nombre est divisible par 7 : on écrit Kizz
+# Si le nombre est divisible par 3 et par 5 : on écrit FizzBuzz
+# Si le nombre est divisible par 3 et par 7 : on écrit FizzKizz
+# Si le nombre est divisible par 5 et par 7 : on écrit BuzzKizz
+# Si le nombre est divisible par 3 et 5 et par 7 : on écrit FizzBuzzKizz
 # Sinon on écrit le nombre
 
-def fizzbuzz(nombre):
+def Fizz(nombre):
     mod3 = nombre % 3
-    mod5 = nombre % 5
-    if mod3==0 and mod5==0 :
-        res="Fizzbuzz"
-    elif mod3 ==0 :
+    if mod3==0:
         res="Fizz"
-    elif mod5 ==0 :
-        res="Buzz"
     else:
-        res=nombre
-
+        res=""
     return res
 
+def Buzz(nombre):
+    mod5 = nombre % 5
+    if mod5==0:
+        res="Buzz"
+    else:
+        res=""
+    return res
+
+def Kizz(nombre):
+    mod7 = nombre % 7
+    if mod7==0:
+        res="Kizz"
+    else:
+        res=""
+    return res
+
+i=0
+
 for nombre in range(101):
-    print(
-        fizzbuzz(nombre)
-    )
+    if bool(Fizz(nombre)) == True or bool(Buzz(nombre)) == True or bool(Kizz(nombre)) == True:
+        print(
+            Fizz(nombre) + Buzz(nombre) + Kizz(nombre)
+        )
+
+    else:
+        print(i)
+    i = i+1
