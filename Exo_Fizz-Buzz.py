f8f06244f20db07174e00c336c1f30623d184686
Yanis LOBASCIO <yanis.lobascio@leboncoin.loc>
2021-06-21 15:21:58 +0200

    Mise en place de la fonction feezbuzz

--- Exo_Fizz-Buzz.py
+++ Exo_Fizz-Buzz.py
@@ -4,19 +4,21 @@
 # Si le nombre est divisible par 3 et par 5 : on écrit Fizzbuzz
 # Sinon on écrit le nombre
 
-nombre = 0 #int(input("Saisissez un nombre : "))
-
-for nombre in range(101):
+def fizzbuzz(nombre):
     mod3 = nombre % 3
     mod5 = nombre % 5
     if mod3==0 and mod5==0 :
         print("Fizzbuzz")
-    
     elif mod3 ==0 :
         print("Fizz")
-    
     elif mod5 ==0 :
         print("Buzz")
-
     else:
         print(nombre)
+
+    return("Fizzbuzz", "Fizz", "Buzz", nombre)
+
+nombre = 0 #int(input("Saisissez un nombre : "))
+
+for nombre in range(101):
+    fizzbuzz(nombre)
